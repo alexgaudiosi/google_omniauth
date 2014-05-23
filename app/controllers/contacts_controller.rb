19b46bfcd47f02b8642712637c817a0d903a2008
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-05-23 12:53:42 +0100

    Sorted contacts alphabetically by name

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,9 @@
 class ContactsController < ApplicationController
 
 	def index
-		@contacts = request.env['omnicontacts.contacts']
+		@contacts = (request.env['omnicontacts.contacts']).sort_by { |k,v| k[:name] }
 		respond_to do |format|
 			format.html 
-		end
-	end
-
-	def contact_callback
-		@contacts = request.env['omnicontacts.contacts']
-		@user = request.env['omnicontacts.user']
-		puts "List of contacts of #{user[:name]} obtained from #params[:importer]}:"
-		@contacts.each do|contact|puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
 		end
 	end
 end
