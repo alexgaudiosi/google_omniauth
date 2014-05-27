a8bfb5c9ce43e27b1a3612b72ce0c9e818b38c1e
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-05-27 11:44:07 +0100

    Update contacts_controller.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
 
 	def index
-		@contacts = ((request.env['omnicontacts.contacts']).sort_by { |k,v| k[:name] }).paginate(:page => params[:page], :per_page => 5)
+		@contacts = ((request.env['omnicontacts.contacts']).sort_by { |k,v| k[:name] }).paginate(:page => params[:page], :per_page => 30)
 		respond_to do |format|
 			format.html 
 		end
